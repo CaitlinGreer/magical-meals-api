30eca39ce93959273352869fcbbf04677e5c3d80
CaitlinGreer <clento1121@gmail.com>
2021-06-02 12:46:01 -0400

    fixed import error

--- src/meals/meals-router.js
+++ src/meals/meals-router.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const xss = require('xss')
 const MealsService = require('./meals-service')
-const express = require('express')
+
 
 const mealsRouter = express.Router()
 const jsonParser = express.json()

daaf045f04a6b23e06e8de2c86e490ecd03aa5f1
CaitlinGreer <clento1121@gmail.com>
2021-06-02 11:11:55 -0400

    created migrate/seed for db

---
+++ seeds/seed.magical_meals.sql
@@ -0,0 +1,6 @@
+INSERT INTO magical_meals (meal_name, meal_description, restaurant_name, meal_location, price, is_vegan, is_vegetarian, is_glutenfree)
+VALUES
+('Vadouvan Spiced Vegetables', 'Sautéed Seasonal Vegetables, Crispy Corn Cake with Coconut-Vadouvan Jus and Pickled Mustard Seeds', 'Be Our Guest', 'Magic Kingdom', 62.00, TRUE, TRUE, FALSE),
+('BBQ Jackfruit Burger', 'Plant-based Burger on Garlic Toast topped with BBQ Jackfruit, Plant-based Mayonnaise and Lettuce served with your choice of side', 'Regal Eagle Smokehosue', 'Epcot', 12.99, TRUE, TRUE, FALSE),
+('Cousin Harolds Vegetable Garden', 'A Seasonal Alternative with all the Fixings', '50s Prime Time Cafe', 'Hollywood Studios', NULL, TRUE, TRUE, FALSE),
+('Hot Link Smokehouse Sandwich', 'Plant-based Sausage topped with a Tangy Mustard Barbecue Sauce, Slaw, and Crispy Onions on a House-made New England-style Roll', 'Flame Tree Barbecue', 'Animal Kingdom', 12.00, TRUE, TRUE, FALSE);

59d69d34a7137752842dc10e3bcd657c5a8e649f
CaitlinGreer <clento1121@gmail.com>
2021-06-03 09:07:49 -0400

    added malicious array

--- test/meal-fixtures.js
+++ test/meal-fixtures.js
@@ -36,6 +36,31 @@ function makeMealArray() {
     ]
 }
 
+function makeMaliciousMeal() {
+    const maliciousMeal = {
+        id: 911,
+        meal_name: 'Test meal with script <script>alert("xss");</script>',
+        meal_description: 'Test description with script <script>alert("xss");</script>',
+        restaurant_name: 'Bad image in content <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. More <strong>content</strong>',
+        meal_location: 'Test location with script <script>alert("xss");</script>',
+        // price: "12.99",
+        // is_vegan: true,
+        // is_vegetarian: true,
+        // is_glutenfree: false,
+    }
+    const expectedMeal = {
+        ...maliciousMeal,
+        meal_name: 'Test meal with script &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+        meal_description: 'Test description with script &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+        restaurant_name: 'Bad image in content <img src="https://url.to.file.which/does-not.exist">. More <strong>content</strong>',
+        meal_location: 'Test location with script &lt;script&gt;alert(\"xss\");&lt;/script&gt;'  
+    }
+    return {
+        maliciousMeal,
+        expectedMeal
+    }
+}
 module.exports = {
-    makeMealArray
+    makeMealArray,
+    makeMaliciousMeal
 }

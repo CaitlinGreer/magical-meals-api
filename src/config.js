3b40f8bdeedb87cadf0ad48d2a4d1c46c4b6f110
CaitlinGreer <clento1121@gmail.com>
2021-06-04 10:01:46 -0400

    troubleshooting migration error

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://caitlin@localhost/magical-meals',
-    TEST_DATABASE_URL: process.env.DATABASE_URL || 'postgresql://caitlin@localhost/magical-meals-test'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://caitlin@localhost/magical-meals?sslmode=no-verify',
+    TEST_DATABASE_URL: process.env.DATABASE_URL || 'postgresql://caitlin@localhost/magical-meals-test?sslmode=no-verify'
   }

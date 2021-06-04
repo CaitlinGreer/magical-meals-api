9da5350bf446d3fe5f827bc25f53d72f45891923
CaitlinGreer <clento1121@gmail.com>
2021-06-04 11:00:20 -0400

    revert changes

--- migrations/001.do.create_magical_meals.sql
+++ migrations/001.do.create_magical_meals.sql
@@ -3,10 +3,10 @@ CREATE TABLE magical_meals (
     meal_name TEXT NOT NULL,
     meal_description TEXT NOT NULL,
     restaurant_name TEXT NOT NULL,
+    meal_location TEXT NOT NULL,
     price NUMERIC,
     is_vegan BOOLEAN,
     is_vegetarian BOOLEAN,
     is_glutenfree BOOLEAN,
-    location_id INTEGER REFERENCES magical_locations(id) ON DELETE CASCADE NOT NULL
 );
 
